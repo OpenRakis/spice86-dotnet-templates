41145abbe495489a4234fbb881f801c77df0a892
Maximilien Noal <noal.maximilien@gmail.com>
2022-09-06 08:56:19 +0200

    First try of a dotnet new template
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ templates/csharp/project/MyOverrideSupplier.cs
@@ -0,0 +1,14 @@
+namespace Spice86ProjectTemplate;
+
+/// <summary>
+/// Provides functions overrides for the DOS program.
+/// </summary>
+public class MyOverrideSupplier : IOverrideSupplier
+{
+    public Dictionary<SegmentedAddress, FunctionInformation> GenerateFunctionInformations(
+        int programStartAddress,
+        Machine machine)
+    {
+        //Add overrides here.
+    }
+}
---
+++ templates/csharp/project/Program.cs
@@ -0,0 +1,9 @@
+﻿// Debug your project with "-e path/to/exe" in your launch settings.
+// Use "-a "some string"" to pass arguments to the emulated program.
+// See https://github.com/OpenRakis/Spice86 for more information
+
+
+// Put the SHA256 checksum of your target DOS program here.
+private const string SUPPORTED_EXE_CHECKSUM = "Your_target_DOS_executable_checksum_here";
+
+Spice86.Program.RunWithOverrides<MyOverrideSupplier>(args, SUPPORTED_EXE_CHECKSUM);
