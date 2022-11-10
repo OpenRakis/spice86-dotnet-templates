02637d0d63cb99aa565b547d91f26897680b6316
Kevin FERRARE <kevin.ferrare@amadeus.com>
2022-11-10 14:04:29 +0100

    Migration to dotnet 7.0

---
+++ templates/csharp/project/GeneratedCode.cs
@@ -0,0 +1,6 @@
+namespace generatedCodeTestProject;
+
+public class GeneratedOverrides : CSharpOverrideHelper {
+  public GeneratedOverrides(Dictionary<SegmentedAddress, FunctionInformation> functionInformations, Machine machine, ushort entrySegment = 0xF000) : base(functionInformations, machine) {
+  }
+}
