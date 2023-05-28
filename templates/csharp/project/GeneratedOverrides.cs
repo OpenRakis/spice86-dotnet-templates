1f50e744acdc4c9b0dd52500460d2e6b6a090609
Maximilien Noal <noal.maximilien@gmail.com>
2023-05-28 12:27:37 +0200

    fix: updated template to the latest package
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- templates/csharp/project/GeneratedOverrides.cs
+++ templates/csharp/project/GeneratedOverrides.cs
@@ -3,6 +3,7 @@ namespace Spice86ProjectTemplate;
 using Spice86.Logging;
 using Spice86.Shared;
 using Spice86.Shared.Interfaces;
+using Spice86.Shared.Emulator.Memory;
 
 public class GeneratedOverrides : CSharpOverrideHelper { 
   public GeneratedOverrides(Dictionary<SegmentedAddress, FunctionInformation> functionInformations, Machine machine, ILoggerService logger, ushort entrySegment = 0xF000) : base(functionInformations, machine, logger) {
--- templates/csharp/project/MyOverrideSupplier.cs
+++ templates/csharp/project/MyOverrideSupplier.cs
@@ -2,6 +2,7 @@ namespace Spice86ProjectTemplate;
 
 using Spice86.Shared;
 using Spice86.Shared.Interfaces;
+using Spice86.Shared.Emulator.Memory;
 using Spice86.Logging;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,11 +10,13 @@ using Microsoft.Extensions.DependencyInjection;
 /// Provides functions overrides for the DOS program.
 /// </summary>
 public class MyOverrideSupplier : IOverrideSupplier {
+    private GeneratedOverrides? _generatedOverrides;
+
     public Dictionary<SegmentedAddress, FunctionInformation> GenerateFunctionInformations(int programStartAddress, Machine machine) {
         Dictionary<SegmentedAddress, FunctionInformation> functionInformations = new();
         // You can extend / replace GeneratedOverrides with your own overrides as well.
         ILoggerService? loggerService = Program.ServiceProvider?.GetService<ILoggerService>();
-        new GeneratedOverrides(functionInformations, machine, loggerService!);
+        _generatedOverrides = new GeneratedOverrides(functionInformations, machine, loggerService!);
         return functionInformations;
     }
 }
