19860b96974c8daffe4304ef992e05c4438ce1e5
Maximilien Noal <noal.maximilien@gmail.com>
2025-11-02 08:03:43 +0100

    chore: Spice86 11.0.0 (template v5.0.0)
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- templates/csharp/project/GeneratedOverrides.cs
+++ templates/csharp/project/GeneratedOverrides.cs
@@ -4,9 +4,16 @@ using Spice86.Core.CLI;
 using Spice86.Shared.Emulator.Memory;
 using Spice86.Shared.Interfaces;
 
-public class GeneratedOverrides : CSharpOverrideHelper { 
-  public GeneratedOverrides(Dictionary<SegmentedAddress, FunctionInformation> functionInformations, Configuration configuration, Machine machine, ILoggerService logger, ushort entrySegment = 0xF000) : base(functionInformations, machine, logger, configuration) {
-  }
+public class GeneratedOverrides : CSharpOverrideHelper
+{
+    private readonly Dictionary<SegmentedAddress, FunctionInformation> _functionsInformation;
+    public GeneratedOverrides(Dictionary<SegmentedAddress, FunctionInformation> functionInformations,
+        Configuration configuration, Machine machine, ILoggerService logger,
+        ushort entrySegment = 0xF000)
+          : base(functionInformations, machine, logger, configuration)
+    {
+        _functionsInformation = functionInformations;
+    }
 
-  public IDictionary<SegmentedAddress, FunctionInformation> FunctionInformation => _functionInformations;
+    public IDictionary<SegmentedAddress, FunctionInformation> FunctionInformation => _functionsInformation;
 }
